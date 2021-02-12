04fdb55c8d77874f7a696155ef5759f1cf1e609d
gcdhg <gcdhg@outlook.com>
2021-02-12 19:58:06 +0300

    now on refresh it generates ul

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
+"use strict";
 textBtn.onclick = async (event) => {
   const res = await fetch("http://localhost:9000/", {
     method: "POST",
     body: text.value,
   });
-  console.log(res);
 };
+
+async function createLIElement(data) {
+  const ul = document.getElementById("tasks");
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(data));
+  ul.appendChild(li);
+}
 
 refr.onclick = async (event) => {
   const res = await fetch("http://localhost:9000/todo/all");
   const json = await res.json();
-  document.getElementById("allTodos").innerHTML = json;
+  await createLIElement(json);
 };
